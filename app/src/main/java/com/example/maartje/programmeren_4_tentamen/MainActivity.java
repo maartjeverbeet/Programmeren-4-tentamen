077f01740eec0e91d1bcf5db343889ca461a9a95
Maartje Verbeet <maartjeverbeet@gmail.com>
2017-06-13 17:53:38 +0200

    hele lelijke login

---
+++ app/src/main/java/com/example/maartje/programmeren_4_tentamen/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.maartje.programmeren_4_tentamen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
