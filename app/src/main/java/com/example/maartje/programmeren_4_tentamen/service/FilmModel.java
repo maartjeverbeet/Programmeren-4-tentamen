e28bf070952d9392627dc8bdb78d27f08def154e
joep <jgjgrute@avans.nl>
2017-06-19 12:21:08 +0200

    initial push

---
+++ app/src/main/java/com/example/maartje/programmeren_4_tentamen/service/FilmModel.java
@@ -0,0 +1,103 @@
+package com.example.maartje.programmeren_4_tentamen.service;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by Joep on 19-06-17.
+ */
+
+public class FilmModel extends AsyncTask<String, Void, String> {
+
+    private OnFilmAvalible listener = null;
+    private String urlString = "";
+    private static final String TAG = FilmModel.class.getSimpleName();
+
+    @Override
+    protected String doInBackground(String... params){
+
+        InputStream inputStream = null;
+        int responsCode = -1;
+        String response = "";
+
+        for(String url : params){
+            Log.i(TAG, "doInBackground " + url);
+        }
+
+        try{
+            URL url = new URL(urlString);
+            URLConnection urlConnection = url.openConnection();
+
+            if (!(urlConnection instanceof HttpURLConnection)) {
+                // Url
+                return null;
+            }
+
+            HttpURLConnection httpConnection = (HttpURLConnection) urlConnection;
+            httpConnection.setAllowUserInteraction(false);
+            httpConnection.setInstanceFollowRedirects(true);
+            httpConnection.setRequestMethod("GET");
+            httpConnection.connect();
+
+            responsCode = httpConnection.getResponseCode();
+
+            if (responsCode == HttpURLConnection.HTTP_OK) {
+                inputStream = httpConnection.getInputStream();
+                response = getStringFromInputStream(inputStream);
+            }
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "doInBackground MalformedURLEx " + e.getLocalizedMessage());
+            return null;
+        } catch (IOException e) {
+            Log.e("TAG", "doInBackground IOException " + e.getLocalizedMessage());
+            return null;
+        }
+        return response;
+    }
+
+
+
+    private static String getStringFromInputStream(InputStream inputStream){
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+
+            br = new BufferedReader(new InputStreamReader(inputStream));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public interface OnFilmAvalible {
+        //void OnFilmAvalible(Film film);
+    }
+}
---
+++ app/src/main/java/com/example/maartje/programmeren_4_tentamen/service/RentallistActivity.java
@@ -0,0 +1,15 @@
+package com.example.maartje.programmeren_4_tentamen.service;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.maartje.programmeren_4_tentamen.R;
+
+public class RentallistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rentallist);
+    }
+}
